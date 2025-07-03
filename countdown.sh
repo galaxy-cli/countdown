d5dad2a689d777c4a327f1c2721e40da9d28734d
galaxy-cli <kvnallbright@gmail.com>
2025-07-03 02:53:47 -0400

    Update countdown.sh

--- countdown.sh
+++ countdown.sh
@@ -1,16 +1,28 @@
 #!/bin/bash
-##################
-#                #
-#  countdown.sh  #
-#                #
-##################
+#          ___ ___  _   _ _ __ | |_ __| | _____      ___ __    ___| |
+#         / __/ _ \| | | | '_ \| __/ _` |/ _ \ \ /\ / / '_ \  / __| '_ \
+#        | (_| (_) | |_| | | | | || (_| | (_) \ V  V /| | | |_\__ \ | | |
+#         \___\___/ \__,_|_| |_|\__\__,_|\___/ \_/\_/ |_| |_(_)___/_| |_|
 
-read -p "Countdown(in mintues): " count
+# User inputs integer in minutes
+read -p "Countdown (in minutes): " count
 
-time=$((60*$count))
+# Checks user input for positive integers
+if ! [[ "$count" =~ ^[0-9]+$ ]] || [ "$count" -le 0 ]; then
+  echo "Please enter a positive integer."
+  exit 1
+fi
 
-for i in $(seq $time -1 1); do 
+# Calculate total time in seconds
+time=$((60 * count))
 
- echo -ne "\r$i"; sleep 1
-
+# Countdown timer in 00:00 format
+for ((i=time; i>0; i--)); do
+  mins=$((i / 60))
+  secs=$((i % 60))
+  printf "\r%02d:%02d" "$mins" "$secs"
+  sleep 1
 done
+
+# When timer completes
+echo -e "\nTime's up!"
